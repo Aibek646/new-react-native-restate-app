5f6e222c37d33ad43beb28a5c4d1f4f0928db3cd
aibekramazanov <sunem.family@gmail.com>
2025-12-27 17:17:58 +0500

    third commit

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,9 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    )
-}
-export default Profile
+    const handleLogout = async () => {};
+    return <SafeAreaView></SafeAreaView>;
+};
+export default Profile;
--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -1,13 +1,13 @@
-import {View, Text} from 'react-native'
-import React from 'react'
-import {useLocalSearchParams} from "expo-router";
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
 
 const Property = () => {
-    const {id} = useLocalSearchParams();
+    const { id } = useLocalSearchParams();
     return (
         <View>
             <Text></Text>
         </View>
-    )
-}
-    export default Property
+    );
+};
+export default Property;
